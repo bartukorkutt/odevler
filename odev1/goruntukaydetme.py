0fcf66e0ff03fa2fd8047be2da4ff126f600eec1
Bartu Korkut <bartukorkutt@gmail.com>
2025-10-27 10:43:14 +0300

    odev6

--- odev1/goruntukaydetme.py
+++ odev1/goruntukaydetme.py
@@ -1,12 +1,12 @@
 import cv2 
 
 carimage = cv2.imread("bugatti-centodieci.jpg")
-
 cv2.imshow("Car Image", carimage)
 cv2.waitKey(0)
 cv2.destroyAllWindows
 
 cv2.imwrite("araba.jpg", carimage)
+
 
 
 
